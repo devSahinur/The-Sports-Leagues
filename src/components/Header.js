f3fde874e175a2a826ea27bd3d1586ae2fc810ed
Sahinur Islam <infosahinur@gmail.com>
2021-05-10 01:44:50 +0600

    new banar add

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../style/Header.css';
+
+const Header = () => {
+    return (
+        <div className='Header'>
+            <h1>The Sport Leagues</h1>
+        </div>
+    );
+};
+
+export default Header;
